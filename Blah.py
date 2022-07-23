e0b73212f2f15a7da19736b11d6af9cad202518b
Martin Johnson <42688266+xor1011@users.noreply.github.com>
2022-07-22 16:29:20 -1000

    Update Blah.py

--- Blah.py
+++ Blah.py
@@ -1,17 +1,3 @@
 # use during quizzes
-class A:
-   def a(self):
-       return "Function inside A"
-
-class B:
-   def a(self):
-       return "Function inside B"
-
-class C:
-   pass
-
-class D(C, A, B):
-   pass
-
-d = D()
-print(d.a())
+def test(x):
+   print (x)
